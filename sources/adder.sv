0a100f6149baac725b5186e0da908176a7a4496d
jp6192 <jenish.chodavadiya1@gmail.com>
2025-12-16 22:58:09 -0800

    reset

--- sources/adder.sv
+++ sources/adder.sv
@@ -1,5 +1,3 @@
-`timescale 1ns/1ps
-
 module simple_adder (
     input wire clk,
     input wire [7:0] a,
@@ -7,11 +5,4 @@ module simple_adder (
     output reg [7:0] sum
 );
     // TODO: Implement addition logic
-
-    always @(posedge clk) begin
-        // 8-bit synchronous adder, modulo 256
-       sum <= a + b;
-       
-    end
-
 endmodule
